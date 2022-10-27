9ee098e3acc0fbbf8d00357b15097040c2b2fb14
DouglasDiones <74056023+DouglasDiones@users.noreply.github.com>
2022-10-27 14:47:49 -0300

    Add files via upload
    Criação da main para testar todas funções

--- src/main/java/br/ufv/caf/BibliotecaGrafos/Main.java
+++ src/main/java/br/ufv/caf/BibliotecaGrafos/Main.java
@@ -18,53 +18,96 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        Boolean flag = false;
         Grafo grafo = new Grafo();
         String enderecoArquivo;
+        
+        boolean flag = true;
 
-        int opcaoMontagem;
+        int opcao,indVerticeEscolhido;
         Scanner in = new Scanner(System.in);
         
-        System.out.println("Digite a opcao que deseja:\n 1- Montagem arquivo\n 2- Montagem dados codigo");
+        System.out.println("Digite o endereço do arquivo que contém o grafo\n");
+        enderecoArquivo = in.nextLine();
         
-        opcaoMontagem = in.nextInt();
-        
-        if(opcaoMontagem == 1){
+ 
 
             // TODO: 22/10/2022 - Manter só entrada do endereço pelo terminal
 //            enderecoArquivo = in.next();
 
             //enderecoArquivo = "/home/joao/Documents/UFV/CCF 331 - GRAFOS/TP1/GraphsLib/BibliotecaGrafos/BibliotecaGrafos/ConversaoJson/ExConversao/extxt.txt";
-            enderecoArquivo = "/home/thiagocr/Documentos/grafo1";
+            //enderecoArquivo = "/home/thiagocr/Documentos/grafo1";
 //            enderecoArquivo = "/home/douglas/Desktop/arquivoQuestao11.txt";
 
             grafo.montarGrafo(enderecoArquivo);
 
-        }
-        else{
 
-            grafo.addVertices(5);
-
-            /*grafo.addAresta(1, 2, 1.2);
-            grafo.addAresta(2, 5, 2.3);
-            grafo.addAresta(3, 5, -8.4);
-            grafo.addAresta(3, 4, 0.3);
-            grafo.addAresta(4, 5, 4.6);
-            grafo.addAresta(1, 5, 0.1);*/
-            
-            grafo.addAresta(1, 2, 1.1);
-            grafo.addAresta(1,3,2.3);
-            grafo.addAresta(2, 5, 3.9);
-            grafo.addAresta(2, 3, 3);
-            grafo.addAresta(3, 4, 4.7);
-            grafo.addAresta(4, 5, 6);
-            grafo.addAresta(2, 4, 2);
+        System.out.println("Qual informação sobre o grafo você deseja:");
+        System.out.println("1 - Informações sobre um vertice em específico;");
+        System.out.println("2 - Informações gerais sobre o grafo;");
+        System.out.println("3 - Relizar busca em profundidade;");
+        System.out.println("4 - Caminho minimo/Distância;");
         
+        opcao = in.nextInt();
+        
+        while(flag){
+            switch (opcao) {
+                //informações do vertice
+                case 1 -> {
+                    System.out.println("Qual o vertice escolhido:");
+                    indVerticeEscolhido = in.nextInt();
+                    System.out.println("Informações do vertice:"+indVerticeEscolhido);
+                    System.out.println("Grau: " + grafo.getGrau(indVerticeEscolhido));
+                    System.out.println("Vizinhos: " + grafo.getVizinhos(indVerticeEscolhido));
+                    System.out.println("Excetricidade:" + grafo.calculaExcentricidade(indVerticeEscolhido));
+                    System.out.println("Centralidade: "+ grafo.centralidadeGrafo(indVerticeEscolhido));
+                }
+                //informações do grafo
+                case 2 -> {
+                    System.out.println("Informações do grafo:");
+                    System.out.println("Grafo: ");
+                    grafo.printGrafo();
+                    System.out.println("");
+                    System.out.println("Ordem: " + grafo.getOrdem());
+                    System.out.println("Tamanho: " + grafo.getTamanho());
+                    System.out.println("Sequencia de graus: " + grafo.getSequenciaGraus());
+                    System.out.println("Diametro: " + grafo.calculaDiametro());
+                    System.out.println("Raio: " + grafo.calculaRaio());
+                    grafo.calculaCentro();
+                    System.out.printf("Centro: ");
+                    grafo.getCentro();
+                    grafo.calculaCaminhoMin();
+                    System.out.println("Matriz de caminhos minimos:");
+                    grafo.getL();
+
+                }
+                //busca em profundidade
+                case 3 -> {
+                    System.out.println("Qual o vertice escolhido para a busca:");
+                    indVerticeEscolhido = in.nextInt();
+                    grafo.BuscaProfundidade(indVerticeEscolhido);
+                    System.out.println("sequencia da busca em profundidade: " + grafo.OrdemBusca);
+                    System.out.println("Arestas que não fazem parte da busca: " + grafo.ArestasRetorno);
+
+                }
+                case 4 -> {
+                    grafo.calculaCaminhoMin();
+                    System.out.println("Matriz de caminho minimos:\n");
+                    grafo.getL();
+                }
+                
+                case 5 -> {
+                    flag = false;
+                }
+                default -> System.out.println("Opcao inválida, digite uma opção válida.");
+            }
         }
-
+        
+        
+        /*
+        System.out.println("");
         grafo.printGrafo();
-
         System.out.println("\n");
+        
         System.out.println("Ordem: " + grafo.getOrdem());
         System.out.println("Tamanho: " + grafo.getTamanho());
         System.out.println("Vizinhos: " + grafo.getVizinhos(1));
@@ -86,7 +129,8 @@ public class Main {
         
 
         // TODO: 22/10/2022 - Isso pode ficar na função, colocar isso na main não fica legal
-
+        
+/*
         if(grafo.calculaCaminhoMin()) {
             grafo.getL();
             System.out.println();
@@ -101,6 +145,8 @@ public class Main {
         }
         else
             System.out.println("GRAFO COM CICLO!!!");
+*/
     }
+
 
 }
